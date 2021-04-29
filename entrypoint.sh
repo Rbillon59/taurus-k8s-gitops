fd8f72689b900044c8f1037eedf901f67f0dfc27
RomainB <romain.billon596@gmail.com>
2021-04-29 22:41:48 +0200

    Init repo

---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Get project from Dockerfile
+project="${1}"
+bzt_path="/home/bzt/.bzt/jmeter-taurus/bin/"
+jmeter_path="/opt/jmeter/apache-jmeter/bin"
+
+source "/tmp/scenario/${project}/.env"
+
+
+if [ "${MODE}" == "SLAVE" ]; then
+
+    echo "Launching slave pod"
+
+    ## Splitting CSV into equal pieces for all injectors
+
+    nb_digit="${#nb_injectors}"
+    mkdir "/tmp/split"
+
+    for csvFileFull in $(find /tmp/scenario/ -name *.csv)
+        do
+            echo "Processing ${csvFileFull}"
+            csvfile="${csvFileFull##*/}"
+            lines_total=$(cat "${csvFileFull}" | wc -l)
+            split --suffix-length="${nb_digit}" -d -l $((lines_total/nb_injectors)) "${csvFileFull}" "/tmp/split/"
+
+            # Getting the right file 
+            jvm_id=$(echo "$(hostname)" | awk -F "-" '{print $3}')
+            cp "/tmp/split/${jvm_id}" "${jmeter_path}/${csvfile}"
+    done
+
+    echo "Starting $(hostname)"
+    jmeter-server -n -Dserver.rmi.localport=50000 -Dserver_port=1099 -Jserver.rmi.ssl.disable=true
+fi
+
+if [ "${MODE}" == "MASTER" ]; then
+    echo "Launching JMeter master $(hostname)"
+    param_host="-Ghost=${host} -Gport=${port} -Gprotocol=${protocol}"
+    param_user="-Gthreads=${threads} -Gduration=${duration} -Grampup=${rampup}"
+
+    set -x
+
+    slave_list=$(getent ahostsv4 taurus-slaves-svc | cut -d" " -f1 | sort -u | awk -v ORS=, '{print $1}' | sed 's/,$//')
+
+    sed -i 's/hostToSed/'${slave_list}'/g' "${project}.yml"
+
+    bzt -o modules.jmeter.properties.host=${host} -o jmeter.properties.port=${port} -o jmeter.properties.protocol=${protocol} -o jmeter.properties.threads=${threads} -o jmeter.properties.duration=${duration} -o jmeter.properties.rampup=${rampup} "${project}.yml"
+
+    #jmeter ${param_host} ${param_user} --logfile ${project}_$(date +"%F_%H%M%S").jtl --nongui --testfile ${project}.jmx -Dserver.rmi.ssl.disable=true --remotestart $(getent ahostsv4 jmeter-slaves-svc | cut -d" " -f1 | sort -u | awk -v ORS=, '{print $1}' | sed 's/,$//')
+
+
+fi
